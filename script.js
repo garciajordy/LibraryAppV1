4e02d0d4f0f2a9fbf1dbcd14b7e6f1185270f3b5
chrystalme <afam_ifeanyi@live.com>
2021-04-28 13:11:02 +0300

    Add elements to display_books

--- script.js
+++ script.js
@@ -1,36 +1,93 @@
-let myLibrary = [];
-let para = document.createElement("p")
-let body = document.getElementById("body")
+const myLibrary = [];
+let para = document.createElement("p");
+const body = document.getElementById("body");
 
-addBookToLibrary("title", "author", 500, true)
-addBookToLibrary("othertitle", "otherauthor", 50)
-addBookToLibrary("newtitle", "newauthor", 200, true)
-addBookToLibrary("thetitle", "theauthor", 400, false)
-//displayBooks()
+addBookToLibrary("title", "author", 500, true);
+addBookToLibrary("othertitle", "otherauthor", 50);
+addBookToLibrary("newtitle", "newauthor", 200, true);
+addBookToLibrary("thetitle", "theauthor", 400, false);
+displayBooks();
 
-function Book(title, author, pages, read = false, image = "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg") {
-    this.image = image
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book(
+  title,
+  author,
+  pages,
+  read = false,
+  image = "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"
+) {
+  this.image = image;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    book = new Book(title, author, pages, read)
-    myLibrary.push(book)
+function addBookToLibrary(title, author, pages, read, image) {
+  let book = new Book(title, author, pages, read, image);
+  myLibrary.push(book);
 }
-
 
 function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    card = document.createElement("div");
+    cardImage = document.createElement("img");
+    cardBody = document.createElement("div");
+    cardTitle = document.createElement("h5");
+    listGroup = document.createElement("ul");
+    listGroupItem1 = document.createElement("li");
+    listGroupItem2 = document.createElement("li");
+    cardBodyBottom = document.createElement("div");
+    btnRead = document.createElement("a");
+    btnDestroy = document.createElement("a");
+    cardImage.src = myLibrary[i].image;
+    cardTitle.innerHTML = myLibrary[i].title;
+    listGroupItem1.innerHTML = myLibrary[i].author;
+    listGroupItem2.innerHTML = myLibrary[i].pages;
+    card.classList.add("card", "col-3");
+    cardImage.classList.add("card-img-top");
+    cardBody.classList.add("card-body");
+    cardTitle.classList.add("card-title");
+    listGroup.classList.add("list-group", "list-group-flush");
+    listGroupItem1.classList.add("list-group-item");
+    listGroupItem2.classList.add("list-group-item");
+    cardBodyBottom.classList.add("card-body");
+    btnRead.classList.add("btn", "btn-primary", "mr-2");
+    btnDestroy.classList.add("btn", "btn-danger");
+    btnRead.innerHTML = readBook(myLibrary[i].read);
+    btnDestroy.innerHTML = "Destroy";
+    btnDestroy.addEventListener("click", deleteBook(myLibrary[i]));
+    btnRead.addEventListener("click", clickBtnRead(myLibrary[i]));
+    body.appendChild(card);
+    card.appendChild(cardImage);
+    card.appendChild(cardBody);
+    cardBody.appendChild(cardTitle);
+    card.appendChild(listGroup);
+    listGroup.appendChild(listGroupItem1);
+    listGroup.appendChild(listGroupItem2);
+    card.appendChild(cardBodyBottom);
+    cardBodyBottom.appendChild(btnRead);
+    cardBodyBottom.appendChild(btnDestroy);
+  }
+}
 
-    for (let i = 0; i < myLibrary.length; i++){
-        para = document.createElement("p")
-        para.innerHTML = myLibrary[i].title
-        para.classList.add("text")
-        body.appendChild(para)
-        console.log(myLibrary[i].author)
-        console.log(myLibrary[i].pages)
-        console.log(myLibrary[i].read)
-    }
+function readBook(book) {
+  if (book) {
+    return "Read";
+  } else {
+    return "Not Read";
+  }
+}
+
+function deleteBook(book) {
+  delete book;
+}
+
+function clickBtnRead(book) {
+  let i = myLibrary.indexOf(book);
+  // console.log(i);
+  if (book.read != false) {
+    myLibrary[i].read = false;
+  } else {
+    myLibrary[i].read = true;
+  }
 }
